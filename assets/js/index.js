59b10ca36cf6f0d0abfc33cef9566a1ac53c18f2
pdcourses <freshitpt@gmail.com>
2022-07-31 13:29:07 +0300

    promise

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,31 @@
+// Promise
 
+const promise1 = new Promise(test);
+
+function test(resolve, reject){
+    console.log('Hello, Vasya!');
+    if(reject){
+        new Error('Reject hello vasya')
+    }
+}
+
+// делаем обещание что получим число больше 50 в интервале от 0 до 100
+
+let promiseGetNumBiggerThan50 = new Promise(getNumFrom0To100);
+console.log(promiseGetNumBiggerThan50);
+
+function getNumFrom0To100(resolve, reject){
+    const num = Math.round(Math.random() * 100);
+    console.log(num);
+    if(num >= 50){
+        resolve(num);
+    } else{
+        reject(new Error(`Reject. Number ${num} is less than 50`))
+    }
+}
+
+
+/*
 const btn = document.getElementById('loadUsers');
 
 const store = {
@@ -30,3 +57,4 @@ function loadReject(response){
     console.log(response);
 
 }
+*/
