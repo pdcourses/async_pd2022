982cd5a6c243d8851dca1ff845b50f278a71f9f1
pdcourses <freshitpt@gmail.com>
2022-07-31 11:54:22 +0300

    sppinerm gif, alert

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+/*
+Асинхронный код выполняется сразу после синхронного
+*/
+
+const spinner = document.getElementById('spinner');
+let c = 0;
+
+setInterval(rotateSpinner, 1000 / 50);
+
+function rotateSpinner () {
+    spinner.style.transform = `rotate(${++c}deg)`;
+}
+
+const helloBtn = document.getElementById('hello');
+
+helloBtn.onclick = function () {
+    alert('Hello');
+};
