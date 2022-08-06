ca5fd3de88442c28b72dc5a26b6d8cc9d6e46088
pdcourses <freshitpt@gmail.com>
2022-08-06 10:32:51 +0300

    promise. зависимости

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,60 +1,86 @@
-// Promise
+// 1
+const promise1 = new Promise(fun1);
 
-const promise1 = new Promise(test);
-
-function test(resolve, reject){
-    console.log('Hello, Vasya!');
-    if(reject){
-        new Error('Reject hello vasya')
-    }
+function fun1(resolve, reject){
+    console.log('hello');
 }
 
-// делаем обещание что получим число больше 50 в интервале от 0 до 100
+//2
+const promise2 = new Promise(
+    function(resolve, reject){
+        console.log('hello');
+    }
+);
 
-let promiseGetNumBiggerThan50 = new Promise(getNumFrom0To100);
-console.log(promiseGetNumBiggerThan50);
 
-function getNumFrom0To100(resolve, reject){
-    const num = Math.round(Math.random() * 100);
-    console.log(num);
-    if(num >= 50){
-        resolve(num);
+// обещание об авторизации пользователя isAuthUser
+const isAuth = true;
+const error = null;
+const currentUser = {name: 'Test', surname: 'Testovich'};
+
+const getAuthUserPromise = new Promise(getAuthUser);
+
+function getAuthUser(resolve, reject){
+    if(isAuth){
+        resolve(currentUser);
     } else{
-        reject(new Error(`Reject. Number ${num} is less than 50`))
+        reject(error);
     }
 }
 
+// цепь из 2 обещаний
+//1
+const isIHaveEnoughMoney = true;
+const buyNewPhonePromise = new Promise(getPhone);
+function getPhone(resolve, reject){
+    if(isIHaveEnoughMoney){
+        const phone = {
+            model: 'modelX',
+            color: 'black',
+            year: 2022
+        };
+        resolve(phone);
+    } else{
+        const reason = new Error('Try again...)');
+        reject(reason);
+    }
+}
+//2
+async function showMyNewCoolPhone(phone){
+    const showNewPhonePromise = new Promise(showForFriends);
+    function showForFriends(resolve, reject){
+        if(phone){
+            const msg = `Hello, I have a new phone ${phone.model} ${phone.year}`;
+            resolve(msg);
+        } else {
+            const promiseMsg = 'I will make this again';
+            reject(promiseMsg);
+        }
+    }
+}
+//connect ES7
+async function buy(){
+    try{
+        const phone = await buyNewPhonePromise;
+        const msg = await showMyNewCoolPhone(phone);
+    } catch(error){
+        console.log(error);
+    }
+}
 
+//вызов 
+buy();
+
+// асинхронный вызов
+(async () => {await buy();})();
+
+//ES6
 /*
-const btn = document.getElementById('loadUsers');
-
-const store = {
-    users: null,
-    isFetching: false,
-    error: null,
+const buy = function(){
+    buyNewPhonePromise
+    .then(showMyNewCoolPhone)
+    .catch(error => console.log(error))
 };
 
-btn.onclick = function () {
-    fetch('../../assets/data/users1.json').then(loadResolve).then(logData).catch(loadReject);
-}
-
-// data to console
-function logData(data){
-    store.isFetching = false;
-    store.users = data;
-    console.log(store.users);
-}
-
-// good!
-function loadResolve(response){
-    store.isFetching = true;
-    return response.json();
-}
-
-// error , bad ((
-function loadReject(response){
-    store.error = new Error(`${response.status} ${response.statusText} `);
-    console.log(response);
-
-}
+buy();
 */
